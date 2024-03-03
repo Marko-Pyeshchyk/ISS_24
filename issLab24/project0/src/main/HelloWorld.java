de1d84f2cd826448ff772378817243f98b3ce442
Marko <markopyeshchyk@gmail.com>
2024-03-03 22:00:42 +0100

    aaa

--- issLab24/project0/src/main/HelloWorld.java
+++ issLab24/project0/src/main/HelloWorld.java
@@ -4,6 +4,6 @@ import unibo.basicomm23.utils.CommUtils;
 public class HelloWorld {
         public static void main( String[] args) {
                 System.out.println("Hello world from project0");
-                CommUtils.outblue("Hello world again from project0");
+                CommUtils.outblue("Hello world again from project0!!!");
         }
 }
