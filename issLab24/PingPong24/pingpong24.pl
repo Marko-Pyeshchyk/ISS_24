486b9a65ca31a6f312cca67a16407493dea20248
MarkoPyeshchykUni <marko.pyeshchyk@studio.unibo.it>
2024-03-29 08:05:15 +0100

    ping pong 24 + deploy + test

---
+++ issLab24/PingPong24/pingpong24.pl
@@ -0,0 +1,10 @@
+%====================================================================================
+% pingpong24 description   
+%====================================================================================
+dispatch( ball, ball(N) ). %info exchanged
+%====================================================================================
+context(ctxtest, "localhost",  "TCP", "8014").
+ qactor( ping, ctxtest, "it.unibo.ping.Ping").
+ static(ping).
+  qactor( pong, ctxtest, "it.unibo.pong.Pong").
+ static(pong).
