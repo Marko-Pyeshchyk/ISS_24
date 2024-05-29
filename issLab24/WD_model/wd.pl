b87a3f433aeafc338504947fa532202e4ac59c2c
Marko <markopyeshchyk@gmail.com>
2024-05-29 13:04:01 +0200

    boundary

--- issLab24/WD_model/wd.pl
+++ issLab24/WD_model/wd.pl
@@ -1,23 +1,7 @@
 %====================================================================================
 % wd description   
 %====================================================================================
-dispatch( turnOn, turnOn(N) ).
-dispatch( turnOff, turnOff(N) ).
-dispatch( foto, foto(X) ).
-event( detection, detection(X) ).
-event( detectionEND, detectionEND(X) ).
 %====================================================================================
-context(ctx_bosco, "127.0.0.1",  "TCP", "8080").
-context(ctx_stazione, "localhost",  "TCP", "8000").
- qactor( sonar, ctx_bosco, "it.unibo.sonar.Sonar").
- static(sonar).
-  qactor( led_blu, ctx_bosco, "it.unibo.led_blu.Led_blu").
- static(led_blu).
-  qactor( fotocamera, ctx_bosco, "it.unibo.fotocamera.Fotocamera").
- static(fotocamera).
-  qactor( stazione, ctx_stazione, "it.unibo.stazione.Stazione").
- static(stazione).
-  qactor( elaboratore, ctx_stazione, "it.unibo.elaboratore.Elaboratore").
- static(elaboratore).
-  qactor( led_rosso, ctx_stazione, "it.unibo.led_rosso.Led_rosso").
- static(led_rosso).
+context(ctx1, "localhost",  "TCP", "8080").
+ qactor( wolf, ctx1, "it.unibo.wolf.Wolf").
+ static(wolf).
--- issLab24/coldStorageService/src/it/unibo/system/System.kt
+++ issLab24/coldStorageService/src/it/unibo/system/System.kt
@@ -13,6 +13,8 @@ import kotlinx.coroutines.runBlocking
 import it.unibo.kactor.sysUtil.createActor   //Sept2023
 
 //User imports JAN2024
+import main.resources.tickets.Tickets
+import main.resources.tickets.Values
 
 class System ( name: String, scope: CoroutineScope, isconfined: Boolean=false  ) : ActorBasicFsm( name, scope, confined=isconfined ){
 
@@ -21,6 +23,10 @@ class System ( name: String, scope: CoroutineScope, isconfined: Boolean=false  )
 	}
 	override fun getBody() : (ActorBasicFsm.() -> Unit){
 		//val interruptedStateTransitions = mutableListOf<Transition>()
+		 val tk = Tickets()
+		 
+				var weight = 60
+				val max_weight = 100
 		return { //this:ActionBasciFsm
 				state("s0") { //this:State
 					action { //it:State
@@ -36,13 +42,41 @@ class System ( name: String, scope: CoroutineScope, isconfined: Boolean=false  )
 					action { //it:State
 						if( checkMsgContent( Term.createTerm("store(N)"), Term.createTerm("store(X)"), 
 						                        currentMsg.msgContent()) ) { //set msgArgList
-								answer("store", "store_accepted", "store_accepted(ok)"   )  
+								if(  tk.spaceBooked() + weight <= max_weight  
+								 ){ 
+													val weight_requested = payloadArg(0).toInt()
+													val Ticket = tk.store(weight_requested)
+								answer("store", "store_accepted", "store_accepted($Ticket)"   )  
+								}
+								else
+								 {answer("store", "store_rejected", "store_rejected(no)"   )  
+								 }
 						}
 						//genTimer( actor, state )
 					}
 					//After Lenzi Aug2002
 					sysaction { //it:State
 					}	 	 
+					 transition(edgeName="t11",targetState="handleStore",cond=whenRequest("store"))
+					transition(edgeName="t12",targetState="store_weight",cond=whenDispatch("store"))
+				}	 
+				state("store_weight") { //this:State
+					action { //it:State
+						CommUtils.outred("weight: $weight")
+						if( checkMsgContent( Term.createTerm("store(N)"), Term.createTerm("store(X)"), 
+						                        currentMsg.msgContent()) ) { //set msgArgList
+								
+												val ticket = payloadArg(0)
+												weight += tk.checkValidity(ticket)
+								CommUtils.outred("weight: $weight")
+						}
+						//genTimer( actor, state )
+					}
+					//After Lenzi Aug2002
+					sysaction { //it:State
+					}	 	 
+					 transition(edgeName="t13",targetState="handleStore",cond=whenRequest("store"))
+					transition(edgeName="t14",targetState="store_weight",cond=whenDispatch("store"))
 				}	 
 			}
 		}
